02250ab0f4795a52afc565156a35520fe25fb7b1
davidgerard66 <david.gerard@ed-creatives.fr>
2016-09-13 21:39:57 +0200

    ter

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,9 +9,9 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
+if (isset($_SERVER['HTTP_CLIENT_IP'] || 1==1)
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1','http://cluster006.ovh.net','193.248.134.93', 'fe80::1', '::1'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1','http://cluster006.ovh.net','http://entreprendre-en-espagne.com','193.248.134.93', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
